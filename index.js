38f91100015bc88ea85eb7c02812e051cbde86f7
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-04-20 12:02:16 -0400

    sword and click adcition added

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+/*let greenCharacter = document.createElement('img')
+greenCharacter.src = 'assets/green-character.gif'
+greenCharacter.style.position = 'fixed'
+greenCharacter.style.left = '100px'
+greenCharacter.style.bottom = '100px'
+document.body.append(greenCharacter) */
+
+/*const pineTree = document.createElement('img')
+pineTree.src = 'assets/pine-tree.png'
+pineTree.style.position = 'fixed'
+pineTree.style.left = '450px'
+pineTree.style.bottom = '200px'
+document.body.append(pineTree) */
+//Hopfully using const will not affect the activity
 function newImage(url, left, bottom) {
-  let object = document.createElement("img");
+  const object = document.createElement("img");
   object.src = url;
   object.style.position = "fixed";
   object.style.left = left + "px";
@@ -13,18 +27,14 @@ newImage("assets/tree.png", 200, 300)
 newImage("assets/pillar.png", 350, 100)
 newImage("assets/crate.png", 150, 200)
 newImage("assets/well.png", 500, 425)
-/*let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter) */
 
-//Hopfully using const will not affect the activity
+const sword = document.createElement('img')
+sword.src = 'assets/sword.png'
+sword.style.position = 'fixed'
+sword.style.left = '500px'
+sword.style.bottom = '405px'
+document.body.append(sword) 
 
-/*const pineTree = document.createElement('img')
-pineTree.src = 'assets/pine-tree.png'
-pineTree.style.position = 'fixed'
-pineTree.style.left = '450px'
-pineTree.style.bottom = '200px'
-document.body.append(pineTree) */
+sword.addEventListener('click', function() {
+    sword.remove()
+})
