a34074b26ec488055300cbcef9d3c438e2a119c3
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-04-20 11:33:28 -0400

    1st attempt to creat function

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+function newImage(a,b,c,d){
+    let a = document.createElement('img')
+a.src = 'b'
+a.style.position = 'fixed'
+a.style.left = 'c'
+a.style.bottom = 'd'
+document.body.append(a)
+}
+
 let greenCharacter = document.createElement('img')
 greenCharacter.src = 'assets/green-character.gif'
 greenCharacter.style.position = 'fixed'
 greenCharacter.style.left = '100px'
 greenCharacter.style.bottom = '100px'
 document.body.append(greenCharacter)
-
+//Hopfully using const will not affect the activity
 const pineTree = document.createElement('img')
 pineTree.src = 'assets/pine-tree.png'
 pineTree.style.position = 'fixed'
