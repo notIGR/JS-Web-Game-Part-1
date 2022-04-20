a64027db27af919cd45ad92076d61378a0c21781
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-04-20 11:47:53 -0400

    function will save much time

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 function newImage(url, left, bottom) {
-    let object = document.createElement('img')
-    object.src = url
-    object.style.position = 'fixed'
-    object.style.left = left +'px'
-    object.style.bottom = bottom +'px'
-    document.body.append(object)
-    return object
+  let object = document.createElement("img");
+  object.src = url;
+  object.style.position = "fixed";
+  object.style.left = left + "px";
+  object.style.bottom = bottom + "px";
+  document.body.append(object);
+  return object;
 }
-newImage()
+newImage("assets/pine-tree.png", 450, 200);
+newImage("assets/green-character.gif", 100, 100);
 
-let greenCharacter = document.createElement('img')
+/*let greenCharacter = document.createElement('img')
 greenCharacter.src = 'assets/green-character.gif'
 greenCharacter.style.position = 'fixed'
 greenCharacter.style.left = '100px'
 greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+document.body.append(greenCharacter) */
 
 //Hopfully using const will not affect the activity
 
