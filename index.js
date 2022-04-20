f844b115123d2c83674a320be2ee16cb9ab37fec
Isaac Rodriguez <isaac.bh.rod@gmail.com>
2022-04-20 11:43:36 -0400

    too many variables, keep code simple!

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-function newImage(a,b,c,d){
-    let a = document.createElement('img')
-a.src = 'b'
-a.style.position = 'fixed'
-a.style.left = 'c'
-a.style.bottom = 'd'
-document.body.append(a)
+function newImage(url, left, bottom) {
+    let object = document.createElement('img')
+    object.src = url
+    object.style.position = 'fixed'
+    object.style.left = left +'px'
+    object.style.bottom = bottom +'px'
+    document.body.append(object)
+    return object
 }
+newImage()
 
 let greenCharacter = document.createElement('img')
 greenCharacter.src = 'assets/green-character.gif'
@@ -13,10 +15,12 @@ greenCharacter.style.position = 'fixed'
 greenCharacter.style.left = '100px'
 greenCharacter.style.bottom = '100px'
 document.body.append(greenCharacter)
+
 //Hopfully using const will not affect the activity
-const pineTree = document.createElement('img')
+
+/*const pineTree = document.createElement('img')
 pineTree.src = 'assets/pine-tree.png'
 pineTree.style.position = 'fixed'
 pineTree.style.left = '450px'
 pineTree.style.bottom = '200px'
-document.body.append(pineTree)
+document.body.append(pineTree) */
